f2e5b1c8ba8baa68ef687e97c93da13548b7aeb2
Emanuel <Emanuel0boehm@gmail.com>
2023-02-28 11:18:14 +0100

    Add heights to Grass Spawner

--- src/grass_spawner.rs
+++ src/grass_spawner.rs
@@ -5,6 +5,7 @@ use crate::height_map::HeightMap;
 pub struct GrassSpawner {
     positions_xz: Vec<Vec2>,
     positions_y: Vec<f32>,
+    heights: HeightRepresentation,
     height_map: Option<HeightMap>,
     _density_map: Option<Handle<Image>>,
     flags: u32,
@@ -71,6 +72,36 @@ impl GrassSpawner {
         self.validate();
         self
     }
+    pub fn with_heights(mut self, heights: Vec<f32>) -> GrassSpawner {
+        assert!(!heights.is_empty());
+
+        let mut flags = GrassSpawnerFlags::from_bits(self.flags).unwrap();
+        if flags.contains(GrassSpawnerFlags::HEIGHT_DEFINED) {
+            panic!("Can not insert heights to `GrassSpawner` since the heights are already defined");
+        }
+
+        flags.insert(GrassSpawnerFlags::HEIGHT_DEFINED);
+
+        self.flags = flags.bits();
+        self.heights = HeightRepresentation::PerBlade(heights);
+
+        self.validate();
+        self
+    }
+    pub fn with_height_uniform(mut self, uniform_height: f32) -> GrassSpawner {
+
+        let mut flags = GrassSpawnerFlags::from_bits(self.flags).unwrap();
+        if flags.contains(GrassSpawnerFlags::HEIGHT_DEFINED) {
+            panic!("Can not insert heights to `GrassSpawner` since the heights are already defined");
+        }
+
+        flags.insert(GrassSpawnerFlags::HEIGHT_DEFINED);
+
+        self.flags = flags.bits();
+        self.heights = HeightRepresentation::Uniform(uniform_height);
+
+        self
+    }
     pub fn with_height_map(mut self, height_map: HeightMap) -> GrassSpawner {
         let mut flags = GrassSpawnerFlags::from_bits(self.flags).unwrap();
         if flags.contains(GrassSpawnerFlags::Y_DEFINED) {
@@ -88,16 +119,34 @@ impl GrassSpawner {
         if !self.positions_xz.is_empty() && !self.positions_y.is_empty() {
             assert_eq!(self.positions_xz.len(), self.positions_y.len());
         }
+        if let HeightRepresentation::PerBlade(heights) = &self.heights {
+            if !self.positions_y.is_empty() {
+                assert_eq!(heights.len(), self.positions_y.len());
+            }
+            if !self.positions_xz.is_empty() {
+                assert_eq!(heights.len(), self.positions_xz.len());
+            }
+        }
     }
 }
 bitflags::bitflags! {
     #[repr(transparent)]
     pub struct GrassSpawnerFlags: u32 {
-        const Y_DEFINED     = (1 << 0);
-        const XZ_DEFINED    = (1 << 1);
-        const HEIGHT_MAP    = (1 << 2);
-        const DENSITY_MAP   = (1 << 3);
-        const NONE          = 0;
-        const UNINITIALIZED = 0xFFFF;
+        const Y_DEFINED      = (1 << 0);
+        const XZ_DEFINED     = (1 << 1);
+        const HEIGHT_DEFINED = (1 << 2);
+        const HEIGHT_MAP     = (1 << 3);
+        const DENSITY_MAP    = (1 << 4);
+        const NONE           = 0;
+        const UNINITIALIZED  = 0xFFFF;
+    }
+}
+pub enum HeightRepresentation {
+    PerBlade(Vec<f32>),
+    Uniform(f32)
+}
+impl Default for HeightRepresentation {
+    fn default() -> Self {
+        HeightRepresentation::Uniform(1.)
     }
 }
