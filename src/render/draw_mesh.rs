db6590371afcfe6e3308fc39fcdec62688cad4af
Emanuel <Emanuel0boehm@gmail.com>
2023-03-01 11:48:59 +0100

    Adapt Render Command

--- src/render/draw_mesh.rs
+++ src/render/draw_mesh.rs
@@ -7,37 +7,38 @@ use bevy::{
     render::{
         mesh::GpuBufferInfo,
         render_asset::RenderAssets,
-        render_phase::{EntityRenderCommand, RenderCommandResult, TrackedRenderPass},
+        render_phase::{RenderCommandResult, TrackedRenderPass, RenderCommand, PhaseItem},
     },
 };
 
 use super::cache::GrassCache;
 pub(crate) struct DrawMeshInstanced;
 
-impl EntityRenderCommand for DrawMeshInstanced {
+impl <P: PhaseItem>RenderCommand<P> for DrawMeshInstanced {
     type Param = (
         SRes<RenderAssets<Mesh>>,
         SRes<GrassCache>,
-        SQuery<Read<Handle<Mesh>>>,
     );
+    type ViewWorldQuery = ();
+    type ItemWorldQuery = Read<Handle<Mesh>>;
 
     #[inline]
     fn render<'w>(
-        _view: Entity,
-        item: Entity,
-        (meshes, cache, mesh_query): SystemParamItem<'w, '_, Self::Param>,
+        item: &P,
+        _view: (),
+        mesh_handle: &'w Handle<Mesh>,
+        (meshes, cache): SystemParamItem<'w, '_, Self::Param>,
         pass: &mut TrackedRenderPass<'w>,
     ) -> RenderCommandResult {
-        let mesh_handle = mesh_query.get(item).unwrap();
-        let gpu_mesh = match meshes.into_inner().get(mesh_handle) {
+        let gpu_mesh = match meshes.get(mesh_handle) {
             Some(gpu_mesh) => gpu_mesh,
             None => return RenderCommandResult::Failure,
         };
-
-        if !cache.contains_key(&item) {
+        let entity = item.entity();
+        if !cache.contains_key(&entity) {
             return RenderCommandResult::Failure;
         }
-        let chunk = &cache.into_inner()[&item];
+        let chunk = &cache.into_inner()[&entity];
         // set uniforms
         pass.set_bind_group(2, chunk.uniform_bindgroup.as_ref().unwrap(), &[]);
         pass.set_bind_group(3, chunk.height_map.as_ref().unwrap(), &[]);
