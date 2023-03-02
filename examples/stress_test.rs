87df4043948332a97fbd9f07aa4cd707c32b2652
Emanuel <Emanuel0boehm@gmail.com>
2023-03-02 12:39:30 +0100

    better fps measure in stresstest

--- examples/stress_test.rs
+++ examples/stress_test.rs
@@ -1,5 +1,5 @@
 use bevy::{
-    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin, Diagnostics, Diagnostic},
     prelude::*,
     window::PresentMode,
 };
@@ -16,17 +16,32 @@ fn main() {
         }))
         .add_plugin(WarblersPlugin)
         .add_plugin(LogDiagnosticsPlugin::default())
-        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(helper::SimpleCamera)
-        .add_startup_system(setup_grass)
+        .add_startup_system(setup_grass)       
+        .add_startup_system(setup_fps)
+        .add_system(diagnostic_system)
         .run();
 }
+pub fn setup_fps(mut diagnostics: ResMut<Diagnostics>) {
+    diagnostics.add(Diagnostic::new(FrameTimeDiagnosticsPlugin::FPS, "fps", 10000));
+}
+pub fn diagnostic_system(
+    mut diagnostics: ResMut<Diagnostics>,
+    time: Res<Time>,
+) {
+    let delta_seconds = time.raw_delta_seconds_f64();
+    if delta_seconds == 0.0 {
+        return;
+    }
+    diagnostics.add_measurement(FrameTimeDiagnosticsPlugin::FPS, || 1.0 / delta_seconds);
+}
+
 fn setup_grass(mut commands: Commands) {
-    let positions = (0..5_000_000)
+    let positions = (0..10_000_000)
         .into_iter()
         .map(|i| {
             let i = i as f32;
-            (i % 1000., i / 1000.)
+            (i % 2000., i / 1000.)
         })
         .map(|(x, z)| Vec3::new(x / 10., 2., z / 10.))
         .collect();
