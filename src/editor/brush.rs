6576f23a90a7b4f35fd2fb13e1d655fc284b2b6a
Emanuel <Emanuel0boehm@gmail.com>
2023-03-09 20:51:38 +0100

    make computed brush size dependend on img size

--- src/editor/brush.rs
+++ src/editor/brush.rs
@@ -56,24 +56,7 @@ pub trait Brush: Sync + Send {
     /// position should be between 0 and 1
     fn draw(&mut self, image: &mut Image, position: Vec2);
 
-    fn pixel_positions(&self, image_dimensions: Vec2, position: Vec2) -> Vec<(u32, u32)> {
-        let position = (image_dimensions * position).as_ivec2();
-        let range = self.size() as i32 / 2;
-        (-range..range)
-            .flat_map(|i| (-range..range).map(move |j| (i, j)))
-            .filter(|(x, y)| {
-                position.y.checked_add(*y).is_some() && position.x.checked_add(*x).is_some()
-            })
-            .map(|(x, y)| (x + position.x, y + position.y))
-            .filter(|(x, y)| {
-                *x >= 0
-                    && *y >= 0
-                    && *x < image_dimensions.x as i32
-                    && *y < image_dimensions.y as i32
-            })
-            .map(|(x, y)| (x as u32, y as u32))
-            .collect()
-    }
+
     fn size(&self) -> u32;
 }
 impl Brush for Stencil {
@@ -84,7 +67,7 @@ impl Brush for Stencil {
         };
         let mut buffer = dynamic_image.into_rgba8();
 
-        for (x, y) in self.pixel_positions(image.size(), position).into_iter() {
+        for (x, y) in pixel_positions(self.size(), image.size(), position).into_iter() {
             let pixel = &mut buffer.get_pixel_mut(x as u32, y as u32).0;
             paint_gray(pixel, self.strength);
         }
@@ -98,7 +81,24 @@ impl Brush for Stencil {
         self.size
     }
 }
-
+fn pixel_positions(brush_size: u32, image_dimensions: Vec2, position: Vec2) -> Vec<(u32, u32)> {
+    let position = (image_dimensions * position).as_ivec2();
+    let range = brush_size as i32 * (image_dimensions.x + image_dimensions.y) as i32 / 100 ;
+    (-range..range)
+        .flat_map(|i| (-range..range).map(move |j| (i, j)))
+        .filter(|(x, y)| {
+            position.y.checked_add(*y).is_some() && position.x.checked_add(*x).is_some()
+        })
+        .map(|(x, y)| (x + position.x, y + position.y))
+        .filter(|(x, y)| {
+            *x >= 0
+                && *y >= 0
+                && *x < image_dimensions.x as i32
+                && *y < image_dimensions.y as i32
+        })
+        .map(|(x, y)| (x as u32, y as u32))
+        .collect()
+}
 fn paint_gray(pixel: &mut [u8; 4], strength: f32) {
     if strength >= 0. {
         let strength = strength as u8;
