b02f037e8ea9a0d9143aee52aa404f8dbfb11503
Emanuel <Emanuel0boehm@gmail.com>
2023-04-08 13:01:31 +0200

    update change_colors example

--- examples/grass_colors.rs
+++ examples/grass_colors.rs
@@ -8,14 +8,6 @@ fn main() {
         // Just a helper plugin for spawning a camera
         // As in all examples, you can use the wasd keys for movement and qe for rotation
         .add_plugin(helper::SimpleCamera)
-        // We can define the color at startup if we want to
-        .insert_resource(GrassConfiguration {
-            // The color of the upper part of the grass blades
-            main_color: Color::WHITE,
-            // The color of the lower part
-            bottom_color: Color::BLACK,
-            ..default()
-        })
         .add_startup_system(setup_grass)
         .add_system(change_colors)
         .run();
@@ -34,16 +26,25 @@ fn setup_grass(mut commands: Commands, asset_server: Res<AssetServer>) {
         density_map,
         height_map,
         aabb: Aabb::from_min_max(Vec3::ZERO, Vec3::new(100., 10., 100.)),
+        // you can define the color for each grass chunk
+        grass_color: GrassColor {
+            main_color: Color::DARK_GREEN,
+            bottom_color: Color::DARK_GREEN * 0.5,
+        },
         ..default()
     },));
 }
 // we can also change the color over at other times
 // this can be useful if your game has seasons
-fn change_colors(mut config: ResMut<GrassConfiguration>, time: Res<Time>) {
+fn change_colors(mut grass_colors: Query<&mut GrassColor>, time: Res<Time>) {
     // Most likely you'd want to choose other colors
     let r = ((time.raw_elapsed_seconds() / 2.).sin() / 2.) + 0.5;
     let g = 1. - r;
-    config.main_color.set_r(r);
-    config.main_color.set_g(g);
-    config.main_color.set_b((g * r).sin());
+    for mut color in &mut grass_colors {
+        color.main_color.set_r(r);
+        color.main_color.set_g(g);
+        color.main_color.set_b((g * r).sin());
+        // the bottom color should normally be a bit darker than the main color.
+        color.bottom_color = color.main_color * 0.5;
+    }
 }
